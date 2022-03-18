a2a2044b43ce39de5bb2afcc1e579d616eb967d4
okereke helen <65390055+helen751@users.noreply.github.com>
2022-03-18 21:34:13 +0100

    Update functions.php

--- user/functions.php
+++ user/functions.php
@@ -2090,7 +2090,7 @@ $htmlContent = '
         <div style="margin-top:2%; text-align:center">
         Hello '.$username.' <br> Here is your password reset Link. Click on the link below or copy and paste in a browser.
         <b>This link can only be used once and expires within a day</b><br>
-         <a href="https://cryptochainspot.com/user/reset?user='.$id2.'&link='.$code2.'"> https://cryptochainspot.com/user/reset?user='.$id2.'&link='.$code2.'</a>
+         
         </div>
         </div>
         
@@ -2144,7 +2144,7 @@ $htmlContent = '
         <div style="margin-top:2%; text-align:center">
         Hello '.$username.' <br> Here is your password reset Link. Click on the link below or copy and paste in a browser.
         <b>This link can only be used once and expires within a day</b><br>
-        <a href="https://cryptochainspot.com/user/reset?user='.$id2.'&link='.$code2.'"> https://cryptochainspot.com/user/reset?user='.$id2.'&link='.$code2.'</a>
+       
         </div>
         </div>
         
