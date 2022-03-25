cc603489e3262c56a1d15c78902b0abbcbe74049
okereke helen <65390055+helen751@users.noreply.github.com>
2022-03-26 00:34:22 +0100

    Update userfooter.php

--- user/userfooter.php
+++ user/userfooter.php
@@ -72,6 +72,7 @@ s0.parentNode.insertBefore(s1,s0);
     fetch('https://coinlib.io/api/v1/coin?key=511d043a7ff82b14&pref=USD&symbol=BTC')
   .then(response => response.json())
   .then(response => {
+    alert(response.price);
 document.getElementById("ta").innerHTML = response.price+" BTC";
 document.getElementById("tb").innerHTML = Math.round(parseFloat(tb)/response.price)+" BTC";
 document.getElementById("td").innerHTML = Math.round(parseFloat(td)/response.price)+" BTC";
