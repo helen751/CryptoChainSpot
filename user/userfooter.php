6c2268d75ee2163f69ee49b2ff206061186dbeb9
okereke helen <65390055+helen751@users.noreply.github.com>
2022-04-09 13:17:58 +0100

    u

--- user/userfooter.php
+++ user/userfooter.php
@@ -15,19 +15,19 @@ function googleTranslateElementInit() {
 </script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     
-   <!--Start of Tawk.to Script-->
+  <!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/62502da1c72df874911e2266/1g04j4ge8';
+s1.src='https://embed.tawk.to/6251788b7b967b11798992b0/1g073tru9';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-<!--End of Tawk.to Script-->    
+<!--End of Tawk.to Script--> 
   <!-- plugins:js -->
   <script src="vendors/js/vendor.bundle.base.js"></script>
   <!-- endinject -->
